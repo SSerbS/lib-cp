b060ac787dc051fc901d5d726c76096213fdd11c
Guilherme Laurentino <144054711+GuiLaurentino@users.noreply.github.com>
2026-03-26 16:09:40 -0300

    Add time complexity comment to Miller-Rabin test
    Added a comment explaining the time complexity and parameters of the Miller-Rabin Primality Test.

---
+++ Number-Theory/MillerRabinPrimalityTest.cpp
@@ -0,0 +1,93 @@
+/*Time Complexity: O(T⋅k⋅logn)
+T: é o número de casos de teste.
+k: é o número de bases primas utilizadas no vetor bases (neste caso, k=12).
+n: é o valor que está sendo testado.
+*/
+vector<ll> bases = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+
+ll mul_mod(ll a, ll b, ll mod)
+{
+    return ((__int128)a % mod * (__int128)b % mod) % mod;
+}
+
+ll fexp(ll a, ll b, ll mod)
+{
+    ll ret = 1;
+
+    while (b > 0)
+    {
+        if (b & 1)
+            ret = mul_mod(a, ret, mod);
+        a = mul_mod(a, a, mod);
+        b >>= 1;
+    }
+
+    return ret;
+}
+
+bool miillerTest(ll a, ll d, ll n)
+{
+    ll x = fexp(a, d, n);
+
+    if (x == 1 || x == n - 1)
+        return true;
+
+    while (d != n - 1)
+    {
+        x = mul_mod(x, x, n);
+        d *= 2;
+
+        if (x == 1)
+            return false;
+        if (x == n - 1)
+            return true;
+    }
+
+    return false;
+}
+
+bool is_prime(ll n)
+{
+    if (n <= 1 || n == 4)
+        return false;
+    if (n <= 3)
+        return true;
+
+    ll d = n - 1;
+
+    while (d % 2 == 0)
+    {
+        d >>= 1;
+    }
+
+    for (int a : bases)
+    {
+        if (a == n)
+            break;
+        if (!miillerTest(a, d, n))
+            return false;
+    }
+    return true;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    cout << is_prime(n) << "\n";
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}

97aedd9b8726b3f1a2cffe195f71660ff8ab6d66
Guilherme Laurentino <144054711+GuiLaurentino@users.noreply.github.com>
2026-03-26 15:50:56 -0300

    Refactor main function and improve query handling
    Refactor main function into solve function and update logic for handling queries and prefix sums.

---
+++ buscas/SweepLine.cpp
@@ -0,0 +1,39 @@
+void solve()
+{
+    int n, q;
+    cin >> n >> q;
+    vector<int> contador(MAXN, 0);
+    vector<pair<int, int>> cordenadas;
+    vector<int> v;
+    vector<ll> psum(1, 0);
+    for (int i = 0; i < n; i++)
+    {
+        int l, r;
+        cin >> l >> r;
+        v.push_back(l);
+        v.push_back(r + 1);
+        cordenadas.push_back({l, r + 1});
+    }
+
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+
+    for (auto [l, r] : cordenadas)
+    {
+        int pos1 = lower_bound(v.begin(), v.end(), l) - v.begin();
+        int pos2 = lower_bound(v.begin(), v.end(), r) - v.begin();
+
+        contador[pos1]++;
+        contador[pos2]--;
+    }
+    for (int i = 1; i <= contador.size(); i++){
+          psum.push_back(psum[psum.size() - 1] + contador[i]);
+    }
+    while(q--){
+        ll pos; cin >> pos;  
+        //Acessamos o valor das queries com o limite superior - 1.
+        cout << prefixsum[(upper_bound(v.begin(), v.end(), pos) - v.begin()) - 1] << "\n";
+
+    }
+    return 0;
+}

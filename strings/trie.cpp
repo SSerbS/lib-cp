c1856fe25a616a84ba425682dab81c1faef71110
Guilherme Laurentino <144054711+GuiLaurentino@users.noreply.github.com>
2026-03-26 13:03:18 -0300

    Implement Trie data structure with insert and prefix count

---
+++ strings/trie.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TrieNode
+{
+    TrieNode *children[26];
+    int is_leaf;
+    int count;
+
+    TrieNode()
+    {
+        is_leaf = 0;
+        count = 0;
+        for (int i = 0; i < 26; i++)
+            children[i] = nullptr;
+    }
+};
+
+void insert(TrieNode *root, const string &key)
+{
+    TrieNode *curr = root;
+    for (char c : key)
+    {
+        if (curr->children[c - 'a'] == nullptr)
+        {
+            TrieNode *newNode = new TrieNode();
+            curr->children[c - 'a'] = newNode;
+        }
+        curr = curr->children[c - 'a'];
+        curr->count++;
+    }
+    curr->is_leaf++;
+}
+
+int num_prefix(TrieNode *root, const string &key)
+{
+    TrieNode *curr = root;
+    for (char c : key)
+    {
+        if (curr->children[c - 'a'] == nullptr)
+            return 0;
+        curr = curr->children[c - 'a'];
+    }
+    return curr->count;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    TrieNode *root = new TrieNode();
+    int n, q;
+    cin >> n >> q;
+
+    for (int i = 0; i < n; i++)
+    {
+        string str;
+        cin >> str;
+        insert(root, str);
+    }
+
+    while (q--)
+    {
+        string str;
+        cin >> str;
+        cout << num_prefix(root, str) << "\n";
+    }
+
+    return 0;
+}

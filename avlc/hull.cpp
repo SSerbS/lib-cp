7d50f7c21c8a9aa9c994d9d66931939409339b4c
SAMUEL SERBIM SOARES <125083598+SSerbS@users.noreply.github.com>
2026-03-26 16:34:45 -0300

    Create hull.cpp

---
+++ avlc/hull.cpp
@@ -0,0 +1,42 @@
+//atenção ao caso de área 0 (todos colineares), números aparecerão repetidos (usar erase+unique)
+
+vector<Point> convexHull(vector<Point>& pts, bool sorted = false){ // pts.size() >= 3
+    int n = pts.size();
+    if(!sorted){
+        sort(begin(pts),end(pts));
+    }
+    vector<Point> lower(n + 1), upper(n + 1);
+    int s = 0;
+    for(int i = 0; i < n; i++){
+        lower[s++] = pts[i];
+        while(s >= 3){
+            Point a = lower[s-3], b = lower[s-2], c = lower[s-1];
+            Point v1 = b-a, v2 = c-b;
+            if(cross(v1,v2) > 0){ // >= se quiser colineares
+                break;
+            }
+            lower[s-2] = lower[s-1];
+            s--;
+
+        }
+    }
+    lower.resize(s);
+    s = 0;
+    for(int i = 0; i < n; i++){
+        upper[s++] = pts[i];
+        while(s >= 3){
+            Point a = upper[s-3], b = upper[s-2], c = upper[s-1];
+            Point v1 = b-a, v2 = c-b;
+            if(cross(v1,v2) < 0){ // <= se quiser colineares
+                break;
+            }
+            upper[s-2] = upper[s-1];
+            s--;
+        }
+    }
+    upper.resize(s-1);
+    reverse(begin(upper),end(upper));
+    upper.pop_back();
+    lower.insert(end(lower),begin(upper),end(upper));
+    return lower;
+}

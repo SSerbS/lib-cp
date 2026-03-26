e23eed8f71d1c5b4fe87baa72e53e38cfb42ab59
SAMUEL SERBIM SOARES <125083598+SSerbS@users.noreply.github.com>
2026-03-26 15:52:26 -0300

    Create getdivisors.cpp

---
+++ Number-Theory/getdivisors.cpp
@@ -0,0 +1,14 @@
+// complexidade temporal: O(sqrt(n))
+// complexidade espacial: O(qntdivisores(n))
+
+vector<int> getdivisors(int n) {
+    vector<int> divisors;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            divisors.push_back(i);
+            if (i != n/i) divisors.push_back(n / i);
+        }
+    }
+
+    return divisors
+}

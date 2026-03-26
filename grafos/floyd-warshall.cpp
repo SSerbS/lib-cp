a33f597800c8063fe6b74b8ac7c8cbd70ad8be5d
SAMUEL SERBIM SOARES <125083598+SSerbS@users.noreply.github.com>
2026-03-26 14:52:47 -0300

    Update floyd-warshall.cpp

--- grafos/floyd-warshall.cpp
+++ grafos/floyd-warshall.cpp
@@ -1,3 +1,13 @@
+/*Na K-ésima iteração gera os
+menores caminhos entre pares
+(u, v) que somente passem por
+um subconjunto de {1, 2, …, K}.
+(note que o subconjunto pode ser o conjunto vazio)
+No final, terá considerado todos
+os vértices do grafo.
+Complexidade: O(V³)
+*/
+
 vvll dist(n+1, vll(n+1));
 
 for (int k = 0; k < n; ++k) {

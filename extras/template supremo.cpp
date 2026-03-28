678b353a2c264345ba67a25603e1d9cd576805c8
SAMUEL SERBIM SOARES <125083598+SSerbS@users.noreply.github.com>
2026-03-27 21:32:24 -0300

    Create template supremo.cpp

---
+++ extras/template supremo.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <sstream>
+#include <limits>
+#include <numeric>
+#include <bitset>
+#include <cstdio>
+
+using namespace std;
+using ll = long long;
+using vvi = vector<vector<int>>;
+using vvpii = vector<vector<pair<int,int>>>;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+using vvb = vector<vector<bool>>;
+using pii = pair<int,int>;
+using tiii = tuple<int,int,int>;
+using pll = pair<ll,ll>;
+//using u128 = unsigned __int128;
+const int MAXN = 5e5 + 5;
+const int inf = 1e9 + 5;
+#define FOR(n) for(int i = 0; i < n; i++)
+
+const int mod = 1e9 + 7;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    
+    
+}
